7c3ca9e2f9aaf330bd53f6c1151a5db1ec3b5910
Tyler D <tyler@tdaigle.com>
2023-02-15 07:02:05 -0500

    offset view

--- src/hexView.ts
+++ src/hexView.ts
@@ -1,5 +1,78 @@
 export function setHexView(hexDiv: HTMLDivElement, data: number[]) {
-    let str = "";
-    data.forEach(d => d < 16 ? str += "0" + d.toString(16).toUpperCase() : str += d.toString(16).toUpperCase());
-    hexDiv.innerHTML = "<p>" + str + "</p>"
+
+    hexDiv.innerHTML = "";
+
+    // convert the raw data into a string of hex digits.
+    // Slice the string array into 16 piece chunks and create <tr>s from them.
+    const hexStrings = convertToHex(data);
+    const { table, tbody } = createHexTable();
+    const sliceSize = 16;
+
+    let currData = hexStrings.slice(0, sliceSize);
+    let sliceCount = 1;
+
+
+    while (currData.length !== 0) {
+        const row = createHexRow(currData);
+        tbody.appendChild(row);
+        sliceCount += 1;
+        // get next slice
+
+        currData = hexStrings.slice(sliceSize * sliceCount, sliceSize * sliceCount + sliceSize);
+    }
+
+    hexDiv.appendChild(table);
+    // hexDiv.innerHTML = "<p>" + str + "</p>"
+}
+
+// convert the raw numeric data into hex numbers;
+function convertToHex(data: number[]): string[] {
+    let str: string[] = [];
+
+    for (let i = 0; i < data.length; i++) {
+        str.push(padHex(data[i]).toUpperCase());
+    }
+    return str;
+}
+
+// hex values 0xF or less should have a leading 0 padded onto them.
+function padHex(num: number): string {
+    if (num <= 0xf) {
+        return `0${num.toString(16)}`;
+    } else {
+        return num.toString(16);
+    }
+}
+
+function createHexTable(numCols: number = 16) {
+    const table = document.createElement("table");
+    const tbody = document.createElement("tbody");
+    table.appendChild(tbody);
+
+    const headerRow = document.createElement("tr");
+    tbody.appendChild(headerRow);
+
+    for (let i = 0; i < numCols; i++) {
+        const th = document.createElement("th");
+        th.innerHTML = i.toString(16);
+        headerRow.appendChild(th);
+    }
+
+    return { table, tbody };
+}
+
+function createHexRow(hexData: string[]) {
+
+    const tr = document.createElement("tr");
+
+    for (const data of hexData) {
+        const td = document.createElement("td");
+        td.innerHTML = data;
+        td.setAttribute("contenteditable", "true");
+        td.classList.add("hex-view-table-cell");
+        // TODO: add a way to edit 
+        // td.addEventListener("input", () => console.log("changed"));
+        tr.appendChild(td);
+    }
+    return tr;
 }
--- src/main.ts
+++ src/main.ts
@@ -5,85 +5,115 @@ import "./style.css";
 
 (function init() {
 
-  // event listener for when the file selector is changed
-  document.getElementById("file-selector")!.addEventListener("change", (e: Event) => {
-    const fileInput = e.target as HTMLInputElement;
-    if (fileInput.files) {
-      const file = fileInput.files[0];
-      console.log(`Loading: ${file}`);
-      const loadedFile = loadFile(file);
-      selectedFileChanged(loadedFile);
-    } else {
-      throw new Error("unable to select a file");
-    }
-  });
+	// event listener for when the file selector is changed
+	document.getElementById("file-selector")!.addEventListener("change", (e: Event) => {
+		const fileInput = e.target as HTMLInputElement;
+		if (fileInput.files) {
+			const file = fileInput.files[0];
+			console.log(`Loading: ${file}`);
+			const loadedFile = loadFile(file);
+			selectedFileChanged(loadedFile);
+		} else {
+			throw new Error("unable to select a file");
+		}
+	});
 
-  // event listener for testing button
-  document.getElementById("test-button")?.addEventListener("click", () => {
-    testData();
-  })
+	// event listener for testing button
+	document.getElementById("test-button")?.addEventListener("click", () => {
+		testData();
+	})
 })();
 
 
 function fakeData(size: number = 500) {
-  const nums = [];
+	const nums = [];
 
-  for (let i = 0; i < size; i++) {
-    nums.push(Math.floor(Math.random() * 255));
-  }
-  return nums;
+	for (let i = 0; i < size; i++) {
+		nums.push(Math.floor(Math.random() * 255));
+	}
+	return nums;
 }
 
 async function selectedFileChanged(loadedFile: LoadedFile) {
-  let currentSlice: number[] = [];
+	let currentSlice: number[] = [];
 
-  const hexDiv = document.getElementById("hex-view") as HTMLDivElement;
-  const asciiDiv = document.getElementById("ascii-view") as HTMLDivElement;
+	const hexDiv = document.getElementById("hex-view") as HTMLDivElement;
+	const asciiDiv = document.getElementById("ascii-view") as HTMLDivElement;
 
-  if (!hexDiv || !asciiDiv) {
-    throw new Error("One of the html elements for viewing the output doesn't exist!");
-  }
+	if (!hexDiv || !asciiDiv) {
+		throw new Error("One of the html elements for viewing the output doesn't exist!");
+	}
 
-  const { fileName, fileSize, read } = loadedFile;
-  console.log("Filename: ", fileName);
-  console.log("Filesize: ", fileSize);
+	const { fileName, fileSize, read } = loadedFile;
+	console.log("Filename: ", fileName);
+	console.log("Filesize: ", fileSize);
 
-  try {
-    currentSlice = await read(50, 500);
-    sliceChanged(currentSlice, hexDiv, asciiDiv);
-  } catch (e) {
-    console.log(e);
-  }
+	try {
+		currentSlice = await read(50, 500);
+		sliceChanged(currentSlice, hexDiv, asciiDiv);
+	} catch (e) {
+		console.log(e);
+	}
 }
 
 function testData() {
-  const hexDiv = document.getElementById("hex-view") as HTMLDivElement;
-  const asciiDiv = document.getElementById("ascii-view") as HTMLDivElement;
-  let currentSlice = fakeData(500);
-  sliceChanged(currentSlice, hexDiv, asciiDiv);
+	const hexDiv = document.getElementById("hex-view") as HTMLDivElement;
+	const asciiDiv = document.getElementById("ascii-view") as HTMLDivElement;
+	let currentSlice = fakeData(1500);
+	sliceChanged(currentSlice, hexDiv, asciiDiv);
 }
 
 function sliceChanged(slice: number[], hexDiv: HTMLDivElement, asciiDiv: HTMLDivElement) {
-  console.log(slice);
-  setHexView(hexDiv, slice);
-  setAsciiView(asciiDiv, slice);
+	// TODO: make the slice a type that contains the data and the start and end offset.
+	console.log(slice);
+	const offsetDisplay = document.getElementById("offset-view")! as HTMLDivElement;
+
+	setOffsetDisplay(offsetDisplay, 0, 1500);
+	setHexView(hexDiv, slice);
+	setAsciiView(asciiDiv, slice);
 }
 
+function setOffsetDisplay(offsetDiv: HTMLDivElement, startOffset: number, endOffset: number) {
+	const labelSize = 8;
+	const bytesPerLine = 16;
 
+	const ol = document.createElement("ol");
+	ol.classList.add("offset-view-list");
+
+	// clear the div
+	offsetDiv.innerHTML = "";
+
+	for (let currOffset = startOffset; currOffset < endOffset; currOffset += bytesPerLine) {
+		const offsetStr = currOffset.toString(16).toUpperCase();
+		let padding = "";
+
+		if (offsetStr.length < labelSize) {
+			for (let i = 0; i < labelSize - offsetStr.length; i++) {
+				padding += "0";
+			}
+		}
+
+		const li = document.createElement("li");
+		li.innerHTML = padding + offsetStr;
+		li.classList.add("offset-view-list-item");
+		ol.appendChild(li);
+	}
+	offsetDiv.appendChild(ol);
+}
 
 function setAsciiView(asciiDiv: HTMLDivElement, data: number[]) {
 
-  // bounds for the printable ASCII character values (in decimal)
-  const asciiStart = 32;
-  const asciiEnd = 126;
+	// bounds for the printable ASCII character values (in decimal)
+	const asciiStart = 32;
+	const asciiEnd = 126;
 
-  let str = "";
-  data.forEach(d => {
-    if (d >= asciiStart && d <= asciiEnd) {
-      str += String.fromCharCode(d);
-    } else {
-      str += ".";
-    }
-  });
-  asciiDiv.innerHTML = "<p>" + str + "</p>"
+	let str = "";
+	data.forEach(d => {
+		if (d >= asciiStart && d <= asciiEnd) {
+			str += String.fromCharCode(d);
+		} else {
+			str += ".";
+		}
+	});
+	asciiDiv.innerHTML = "<p>" + str + "</p>"
 }
