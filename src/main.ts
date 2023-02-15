5b45799cc19d75d9657dffdd5bb5b05ee9d2ea36
Tyler D <tyler@tdaigle.com>
2023-02-15 04:50:56 -0500

    view functions

--- src/main.ts
+++ src/main.ts
@@ -18,24 +18,39 @@ import "./style.css";
 
 async function selectedFileChanged(loadedFile: LoadedFile) {
   let currentSlice: number[] = [];
+  const hexDiv = document.getElementById("hex-view") as HTMLDivElement;
+  const asciiDiv = document.getElementById("ascii-view") as HTMLDivElement;
+
+  if (!hexDiv || !asciiDiv) {
+    throw new Error("One of the html elements for viewing the output doesn't exist!");
+  }
 
   const { fileName, fileSize, read } = loadedFile;
   console.log("Filename: ", fileName);
   console.log("Filesize: ", fileSize);
 
   try {
-    currentSlice = await read(0, 50);
-    sliceChanged(currentSlice);
+    currentSlice = await read(50, 500);
+    sliceChanged(currentSlice, hexDiv, asciiDiv);
   } catch (e) {
     console.log(e);
   }
 }
 
-function sliceChanged(slice: number[]) {
+function sliceChanged(slice: number[], hexDiv: HTMLDivElement, asciiDiv: HTMLDivElement) {
   console.log(slice);
+  setHexView(hexDiv, slice);
+  setAsciiView(asciiDiv, slice);
+}
+
+function setHexView(hexDiv: HTMLDivElement, data: number[]) {
   let str = "";
+  data.forEach(d => d < 16 ? str += "0" + d.toString(16).toUpperCase() : str += d.toString(16).toUpperCase());
+  hexDiv.innerHTML = "<p>" + str + "</p>"
+}
 
-  slice.forEach(s => str += String.fromCharCode(s));
-
-  console.log(str);
+function setAsciiView(asciiDiv: HTMLDivElement, data: number[]) {
+  let str = "";
+  data.forEach(d => str += String.fromCharCode(d));
+  asciiDiv.innerHTML = "<p>" + str + "</p>"
 }
